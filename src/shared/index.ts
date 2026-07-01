2a623b1327643c5b170b3261297a6e9ea9d06d57
mamahoos <m4m4hoos@gmail.com>
2026-07-01 23:47:46 +0330

    chore(shared): add barrel export for shared kernel modules

---
+++ src/shared/index.ts
@@ -0,0 +1,6 @@
+export * from './errors/index.js';
+export * from './idempotency/index.js';
+export * from './ids/index.js';
+export * from './money/index.js';
+export * from './pagination/index.js';
+export * from './result/index.js';
